22a39e9e74b68be29a532c791a8a5c2e175821f0
Bruno Montebello <brunormontebello@hotmail.com>
2021-03-21 11:44:48 +0000

    Config auth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { LoginButton } from '@inrupt/solid-ui-react';
 import './App.css';
 
+const authOptions = {
+  clientName: 'Solid To-do app',
+};
+
+
 function App() {
+  const [oidcIssuer, setOidcIssuer] = useState('');
+
+  const handleChange = (e) => {
+    setOidcIssuer(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <span>
+        <input type="text"
+               className="oidc-issuer-input"
+               name="oidcIssuer"
+               list="providers"
+               value={oidcIssuer}
+               onChange={handleChange}
+        />
+        <datalist id="providers">
+          <option value="https://broker.pod.inrupt.com/" />
+          <option value="https://inrupt.net/" />
+        </datalist>
+      </span>
+      <LoginButton oidcIssuer={oidcIssuer}
+                   redirectUrl={window.location.href}
+                   authOptions={authOptions}
+      />
+
     </div>
   );
 }
